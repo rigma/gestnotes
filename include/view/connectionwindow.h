c6f4b4e233000c1a12b58caa9c55127b5134a0ab
zubzub <thibault.zuber@free.fr>
2015-05-05 11:46:50 +0200

    Ajout de l'interface de connexion

--- include/view/connectionwindow.h
+++ include/view/connectionwindow.h
@@ -1,7 +1,9 @@
 #ifndef CONNECTIONWINDOW_H
 #define CONNECTIONWINDOW_H
 
+#include <QMap>
 #include <QWidget>
+#include <model/repository.h>
 
 namespace Ui {
 class ConnectionWindow;
@@ -12,11 +14,18 @@ class ConnectionWindow : public QWidget
     Q_OBJECT
 
 public:
-    explicit ConnectionWindow(QWidget *parent = 0);
+    explicit ConnectionWindow(QMap<QString, Repository*> *repositories, QWidget *parent = 0);
     ~ConnectionWindow();
+
+
+private slots:
+    void connect();
 
 private:
     Ui::ConnectionWindow *_ui;
+    QMap<QString, Repository*> *_repositories;
+
+
 };
 
 #endif // CONNECTIONWINDOW_H
--- src/view/connectionwindow.cpp
+++ src/view/connectionwindow.cpp
@@ -1,13 +1,37 @@
+#include <model/administrator_repository.h>
 #include <view/connectionwindow.h>
+
 #include "ui_connectionwindow.h"
 
-ConnectionWindow::ConnectionWindow(QWidget *parent) : QWidget(parent),
-    _ui(new Ui::ConnectionWindow)
+ConnectionWindow::ConnectionWindow(QMap<QString, Repository*> *repositories, QWidget *parent) : QWidget(parent),
+    _ui(new Ui::ConnectionWindow),
+    _repositories(repositories)
 {
     _ui->setupUi(this);
+    QObject::connect(_ui->Button_connect, SIGNAL(clicked()), this, SLOT(connect()));
 }
 
 ConnectionWindow::~ConnectionWindow()
 {
     delete _ui;
+}
+
+
+void ConnectionWindow::connect()
+{
+    AdministratorRepository *repo = (AdministratorRepository*) _repositories->value("administrator");
+    QList<Entity*> result;
+    QMap<QString, QVariant> criteria;
+
+    criteria.insert("serial", QVariant(_ui->lineEdit_id->text()));
+    criteria.insert("passwd", QVariant(qHash(_ui->lineEdit_mdp->text())));
+
+    result = repo->findBy(criteria);
+
+    if (result.empty())
+    {
+        _ui->lineEdit_id->setText(QString(""));
+        _ui->lineEdit_mdp->setText(QString(""));
+    }
+    // else afficher la nouvelle fenêtre
 }
